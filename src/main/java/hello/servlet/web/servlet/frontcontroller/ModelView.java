86a03738e620fe6b892a0ae4e17e562b66222bf5
malvin222 <mahebin@naver.com>
2024-08-12 16:32:20 +0900

    -v3

--- src/main/java/hello/servlet/web/servlet/frontcontroller/ModelView.java
+++ src/main/java/hello/servlet/web/servlet/frontcontroller/ModelView.java
@@ -10,4 +10,20 @@ public class ModelView {
     public ModelView(String viewName){
         this.viewName = viewName;
     }
+
+    public String getViewName() {
+        return viewName;
+    }
+
+    public void setViewName(String viewName) {
+        this.viewName = viewName;
+    }
+
+    public Map<String, Object> getModel() {
+        return model;
+    }
+
+    public void setModel(Map<String, Object> model) {
+        this.model = model;
+    }
 }
